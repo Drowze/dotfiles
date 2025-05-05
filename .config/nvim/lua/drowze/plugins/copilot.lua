a40e58f22a28452d3735fa3f9dd70e48842b3ae8
Rafael Gibim <9031589+Drowze@users.noreply.github.com>
2025-05-05 13:40:42 -0300

    nvim: change copilot lazy load event

--- .config/nvim/lua/drowze/plugins/copilot.lua
+++ .config/nvim/lua/drowze/plugins/copilot.lua
@@ -2,14 +2,22 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    event = "InsertEnter",
+    event = "VeryLazy",
     opts = {
       suggestion = {
         auto_trigger = true,
         keymap = {
           accept = "<Tab>",
         }
-      }
+      },
+      -- logger = {
+      --   file = vim.fn.stdpath("log") .. "/copilot-lua.log",
+      --   file_log_level = vim.log.levels.INFO,
+      --   -- print_log_level = vim.log.levels.INFO,
+      --   trace_lsp = "verbose", -- "off" | "messages" | "verbose"
+      --   trace_lsp_progress = true,
+      --   log_lsp_messages = true,
+      -- }
     }
   },
   {
